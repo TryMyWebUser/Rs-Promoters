1b8dcc7e180037aa93ec9604b81c682f24858f54
TryMyWebsites <trymywebsites@gmail.com>
2025-06-24 16:01:31 +0530

    template commit

---
+++ project.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from expert-themes.com/html/constrc/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 20 Jun 2025 06:31:24 GMT -->
+<head>
+      <?php include 'temp/head.php'; ?>
+</head>
+
+<body>
+
+<div class="page-wrapper">
+	
+	<!-- Cursor -->
+	<div class="cursor"></div>
+	<div class="cursor-follower"></div>
+	<!-- Cursor End -->
+ 	
+	<!-- Preloader -->
+	<!-- <div class="preloader">
+		<div class="loader">
+			<div class="outer-circle"></div>
+			<div class="inner-circle"></div>
+			<div class="dot"></div>
+		</div>
+	</div> -->
+	<!-- End Preloader -->
+	
+	<!-- Main Header -->
+    <?php include 'temp/header.php'; ?>
+	<!-- End Main Header -->
+	
+	<!-- About Sidebar -->
+	<div class="about-sidebar">
+		<div class="gradient-layer"></div>
+		<!-- Close Button -->
+		<div class="close-sidebar-widget close-button">
+			<span class="fa-solid fa-xmark fa-fw"></span>
+		</div>
+		<div class="sidebar-inner">
+			<div class="upper-box">
+				<div class="image">
+					<img src="assets/images/resource/about-1.jpg" alt="" />
+				</div>
+				<div class="content-box">
+					<h3>About <span>Constrc</span></h3>
+					<div class="text">our clients, oue employees, and our community through our commitmrnt to leadership, excellence in craft, and attention to detail.</div>
+					<ul class="about-sidebar_list">
+						<li>Testimonials</li>
+						<li>Outsourcing</li>
+						<li>Privacy Policy</li>
+						<li>HR Training</li>
+						<li>Careers</li>
+					</ul>
+				</div>
+			</div>
+			<!-- Social Box -->
+			<div class="social-box">
+				<a href="https://facebook.com/"><i class="fa-brands fa-facebook-f"></i></a>
+				<a href="https://twitter.com/"><i class="fa-brands fa-twitter"></i></a>
+				<a href="https://youtube.com/"><i class="fa-brands fa-youtube"></i></a>
+				<a href="https://instagram.com/"><i class="fa-brands fa-instagram"></i></a>
+			</div>
+		</div>
+	</div>
+	<!-- End About Sidebar -->
+
+    <!-- Page Title -->
+    <section class="page-title" style="background-image:url(assets/images/background/6.jpg)">
+        <div class="auto-container">
+			<h2>Project</h2>
+			<div class="d-flex justify-content-between align-items-center flex-wrap">
+				<ul class="bread-crumb clearfix">
+					<li><a href="index.html">Home</a></li>
+					<li>Project</li>
+				</ul>
+			</div>
+        </div>
+    </section>
+    <!-- End Page Title -->
+
+	
+	<!-- Main Footer -->
+    <?php include 'temp/footer.php'; ?>
+	<!-- End Main Footer -->
+	
+	<!-- Search Popup -->
+	<div class="search-popup">
+		<div class="color-layer"></div>
+		<button class="close-search"><span class="fa-xmark"></span></button>
+		<form method="post" action="https://expert-themes.com/html/constrc/blog.html">
+			<div class="form-group">
+				<input type="search" name="search-field" value="" placeholder="Search Here" required="">
+				<button class="fa fa-solid fa-magnifying-glass fa-fw" type="submit"></button>
+			</div>
+		</form>
+	</div>
+	<!-- End Search Popup -->
+	
+	<!-- Color Palate / Color Switcher -->
+	
+	<!-- End Color Palate / Color Switcher -->
+	
+</div>
+<!-- End PageWrapper -->
+
+<div class="progress-wrap">
+	<svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+		<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
+	</svg>
+</div>
+
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/appear.js"></script>
+<script src="assets/js/parallax.min.js"></script>
+<script src="assets/js/tilt.jquery.min.js"></script>
+<script src="assets/js/jquery.paroller.min.js"></script>
+<script src="assets/js/wow.js"></script>
+<script src="assets/js/swiper.min.js"></script>
+<script src="assets/js/backtotop.js"></script>
+<script src="assets/js/odometer.js"></script>
+<script src="assets/js/parallax-scroll.js"></script>
+
+<script src="assets/js/gsap.min.js"></script>
+<script src="assets/js/SplitText.min.js"></script>
+<script src="assets/js/ScrollTrigger.min.js"></script>
+<script src="assets/js/ScrollToPlugin.min.js"></script>
+<script src="assets/js/ScrollSmoother.min.js"></script>
+
+<script src="assets/js/jquery.marquee.min.js"></script>
+<script src="assets/js/validate.js"></script>
+<script src="assets/js/typeit.js"></script>
+<script src="assets/js/jquery.marquee.min.js"></script>
+<script src="assets/js/magnific-popup.min.js"></script>
+<script src="assets/js/nav-tool.js"></script>
+<script src="assets/js/jquery-ui.js"></script>
+<script src="assets/js/element-in-view.js"></script>
+<script src="assets/js/color-settings.js"></script>
+<script src="assets/js/script.js"></script>
+
+</body>
+
+<!-- Mirrored from expert-themes.com/html/constrc/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 20 Jun 2025 06:32:05 GMT -->
+</html>
---
+++ template/temp/header.php
@@ -0,0 +1,92 @@
+<header class="main-header header-style-one">
+		
+		<!-- Header Lower -->
+		<div class="header-lower">
+			<div class="auto-container">
+				<div class="inner-container">
+					<div class="d-flex justify-content-between align-items-center">
+						
+						<div class="logo-box">
+							<div class="logo"><a href="index.php"><img src="assets/images/logo-2.png" alt="" title="Constrc" height="39" width="179"></a></div>
+						</div>
+						
+						<div class="nav-outer d-flex flex-wrap">
+							<!-- Main Menu -->
+							<nav class="main-menu navbar-expand-md">
+								<div class="navbar-header">
+									<!-- Toggle Button -->    	
+									<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+								</div>
+								
+								<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+									<ul class="navigation clearfix">
+										<li class="has-mega-menu"><a href="index.php">Home</a></li>
+										<li class=""><a href="about.php">About</a></li>
+										<li class="dropdown"><a href="#">Project</a>
+                                            <ul>
+												<li><a href="project.php">New Land</a></li>
+												<li><a href="project.php">New House</a></li>
+											</ul>
+                                        </li>
+										<li class="dropdown"><a href="#">Our Project</a>
+											<ul>
+												<li><a href="our-project.php">Ongoing</a></li>
+												<li><a href="our-project.php">Completed</a></li>
+											</ul>
+										</li>
+										<li class=""><a href="contact.php">Contact</a></li>
+									</ul>
+								</div>
+							</nav>
+						</div>
+
+						<!-- Main Menu End-->
+						<div class="outer-box d-flex align-items-center flex-wrap">
+
+							<!-- Button Box -->
+							<div class="main-header_button">
+								<a href="tel:+916383766144" class="theme-btn btn-style-three">
+									<span class="btn-wrap">
+										<span class="text-one">GET A QUOTE <i><img src="assets/images/icons/arrow-1.svg" alt="" /></i></span>
+										<span class="text-two">GET A QUOTE <i><img src="assets/images/icons/arrow-1.svg" alt="" /></i></span>
+									</span>
+								</a>
+							</div>
+							
+							<!-- About Widget -->
+							<!-- <span class="about-widget">
+								<span class="hamburger">
+									<span class="top-bun"></span>
+									<span class="meat"></span>
+									<span class="bottom-bun"></span>
+								</span>
+							</span> -->
+							
+							<!-- Mobile Navigation Toggler -->
+							<div class="mobile-nav-toggler"><span class="icon fa-classic fa-solid fa-bars fa-fw"></span></div>
+
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--End Header Lower-->
+		
+		<!-- Mobile Menu  -->
+		<div class="mobile-menu">
+			<div class="menu-backdrop"></div>
+			<div class="close-btn"><span class="icon fa-xmark"></span></div>
+			
+			<nav class="menu-box">
+				<div class="nav-logo"><a href="index.php"><img src="assets/images/mobile-logo.svg" alt="" title=""></a></div>
+				<div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+			</nav>
+		</div>
+		<!-- End Mobile Menu -->
+	
+	</header>
